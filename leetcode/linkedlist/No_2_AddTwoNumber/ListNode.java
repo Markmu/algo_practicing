aed4558854e141f21d54fefac1a7c160acabe73b
MarkMu <muchao1303@gmail.com>
2019-04-28 10:06:42 +0800

    Solve problem [Add Two Number]

---
+++ leetcode/linkedlist/No_2_AddTwoNumber/ListNode.java
@@ -0,0 +1,10 @@
+package com.mark.leetcode.linkedlist.No_2_AddTwoNumber;
+
+/**
+ * Created by Mark on 2019-04-28.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
