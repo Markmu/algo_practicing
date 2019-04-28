ab53c483470c3b63841eaa350434135c70560be0
MarkMu <muchao1303@gmail.com>
2019-04-28 10:11:46 +0800

    Move ListNode class

--- leetcode/linkedlist/ListNode.java
+++ leetcode/linkedlist/ListNode.java
@@ -4,7 +4,7 @@ package com.mark.leetcode.linkedlist.No_2_AddTwoNumber;
  * Created by Mark on 2019-04-28.
  */
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
 }
