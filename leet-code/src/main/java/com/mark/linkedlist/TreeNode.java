168959e9ae05493ea630d8f3b275e96be0148ac0
Mark C. Mu <muchao1303@gmail.com>
2019-07-01 20:33:16 +0800

    Solve problem [Convert Sorted List to BST]

---
+++ leet-code/src/main/java/com/mark/linkedlist/TreeNode.java
@@ -0,0 +1,10 @@
+package com.mark.linkedlist;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode (int val) {
+        this.val = val;
+    }
+}
