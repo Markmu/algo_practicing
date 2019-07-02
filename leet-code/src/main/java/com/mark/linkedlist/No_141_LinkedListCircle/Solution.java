75de690850cee54cc5ee3400b8cf94e5fe0722c0
Mark C. Mu <muchao1303@gmail.com>
2019-07-02 18:25:24 +0800

    Solve problem [Linked list Circle]

---
+++ leet-code/src/main/java/com/mark/linkedlist/No_141_LinkedListCircle/Solution.java
@@ -0,0 +1,38 @@
+package com.mark.linkedlist.No_141_LinkedListCircle;
+
+import com.mark.linkedlist.ListNode;
+
+public class Solution {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        for (int i = 0; i < 5; i++) {
+            cur.next = new ListNode(i);
+            cur = cur.next;
+        }
+        cur.next = head.next.next;
+
+        System.out.println(new Solution().hasCycle(head));
+    }
+
+}
