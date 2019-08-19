1929ed2ddd9bc1a29a0e279e5d095d54c6f6ce5a
Mark C. Mu <muchao1303@gmail.com>
2019-08-19 22:43:48 +0800

    Solve problem [Single Number]

---
+++ leet-code/src/main/java/com/mark/No_136_SingleNumber/Solution.java
@@ -0,0 +1,11 @@
+package com.mark.No_136_SingleNumber;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result  = 0;
+        for (int i : nums) {
+            result ^= i;
+        }
+        return result;
+    }
+}
